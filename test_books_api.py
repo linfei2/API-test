d50840e344fb619dcd1cd3d8d92149dad0a30b21
linfei2 <dominika.matczynska@gmail.com>
2022-03-13 12:42:48 +0100

    Add tests to display all books and a single book

--- test_books_api.py
+++ test_books_api.py
@@ -11,8 +11,14 @@ def test_register_api_client(base_url):
     assert j["accessToken"]
 
 
+def test_get_all_books(base_url):
+    url = base_url + "/books"
+    resp = requests.get(url)
+    assert resp.status_code == 200
 
 
-
-
-
+@pytest.mark.parametrize("book_id", [1, 6])
+def test_get_valid_book(base_url, book_id):
+    url = base_url + f"/books/{book_id}"
+    resp = requests.get(url)
+    assert resp.status_code == 200
