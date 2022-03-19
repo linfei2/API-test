82192ca96267e1005b5e918cd37b4e6e294d52d3
linfei2 <dominika.matczynska@gmail.com>
2022-03-19 17:30:47 +0100

    Add tests for updating and deleting order

--- test_books_api.py
+++ test_books_api.py
@@ -22,8 +22,7 @@ def register_api_client(base_url, random_name, random_email):
     url = base_url + "/api-clients/"
     data = {"clientName": random_name, "clientEmail": random_email}
     response = requests.post(url, json=data)
-    json_response = response.json()
-    return response, json_response["accessToken"]
+    return response, response.json()["accessToken"]
 
 
 def test_register_api_client(register_api_client):
@@ -36,6 +35,7 @@ def test_get_all_books(base_url):
     url = base_url + "/books"
     response = requests.get(url)
     assert response.status_code == 200
+    assert isinstance(response.json(), list)
 
 
 @pytest.mark.parametrize("book_id", [1, 6])
@@ -48,9 +48,8 @@ def test_get_valid_book(base_url, book_id):
 def test_get_invalid_book(base_url, book_id=50):
     url = base_url + f"/books/{book_id}"
     response = requests.get(url)
-    json_response = response.json()
     assert response.status_code == 404
-    assert json_response["error"] == f"No book with id {book_id}"
+    assert response.json()["error"] == f"No book with id {book_id}"
 
 
 @pytest.fixture
@@ -66,8 +65,7 @@ def submit_order(base_url, authorize):
     headers = authorize
     order_details = {"bookId": 1, "customerName": "John"}
     response = requests.post(url, json=order_details, headers=headers)
-    json_response = response.json()
-    order_id = json_response["orderId"]
+    order_id = response.json()["orderId"]
     return response, order_id, headers
 
 
@@ -82,6 +80,7 @@ def test_get_all_orders(base_url, authorize):
     headers = authorize
     response = requests.get(url, headers=headers)
     assert response.status_code == 200
+    assert isinstance(response.json(), list)
 
 
 def test_get_single_order(base_url, submit_order):
@@ -90,3 +89,22 @@ def test_get_single_order(base_url, submit_order):
     response = requests.get(url, headers=headers)
     assert response.status_code == 200
 
+
+def test_update_order(base_url, submit_order):
+    response, order_id, headers = submit_order
+    url = base_url + f"/orders/{order_id}"
+    data = {"customerName": "Jane"}
+    patch_response = requests.patch(url, json=data, headers=headers)
+    get_response = requests.get(url, headers=headers)
+    assert patch_response.status_code == 204
+    assert get_response.json()["customerName"] == "Jane"
+
+
+def test_delete_order(base_url, submit_order):
+    response, order_id, headers = submit_order
+    url = base_url + f"/orders/{order_id}"
+    delete_response = requests.delete(url, headers=headers)
+    get_response = requests.get(url, headers=headers)
+    assert delete_response.status_code == 204
+    assert get_response.status_code == 404
+    assert get_response.json()["error"] == f"No order with id {order_id}"
